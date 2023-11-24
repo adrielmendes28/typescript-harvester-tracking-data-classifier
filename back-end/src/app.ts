e1133b26d45ed2fc51c0ba16953ee2723731734d
Adriel Mendes <adriel.amendes12@gmail.com>
2023-11-24 00:19:52 -0300

    fix: corrige projeto para docker

--- back-end/src/app.ts
+++ back-end/src/app.ts
@@ -9,7 +9,7 @@ const PORT = 3333;
 
 app.use(cors());
 app.use(express.json());
-app.use(routes);
+app.use('/api', routes);
 app.listen(PORT, () => {
   //console.log(`Server is running on port ${PORT}`);
 });
